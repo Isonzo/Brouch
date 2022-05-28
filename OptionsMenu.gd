ab4d8a7ab724f3627d1ff49b3935643cf6166b59
Isonzo <renato.soveina@gmail.com>
2022-05-28 12:45:39 -0400

    Added Menu for keybindings, although text is currently not being set properly

--- OptionsMenu.gd
+++ OptionsMenu.gd
@@ -1,5 +1,21 @@
 extends Control
 
+enum ACTIONS{move_right_, move_left_, jump_, cast_}
+
+func _ready():
+	_set_keys()
+
+func _set_keys():
+	for action in ACTIONS:
+		var input: String = str(action)
+		var path: String = "TabContainer/Keybindings/ScrollContainer/Controls/HBoxCont_" + input + "/Button"
+		var current_action: InputEvent = InputMap.get_action_list(str(action + "1"))[0]
+		var string: String = current_action.as_text()
+		
+		if string.empty():
+			get_node(path).text = "No button"
+		else:
+			get_node(path).text = string
 
 func _on_MainMenu_show_options():
 	visible = true
